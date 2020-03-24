d56c08dd73871d93e58b8842e63182661814d063
Dioma <dioma@gmail.com>
2020-03-24 13:11:12 +0200

    naxody vse

---
+++ main.js
@@ -0,0 +1,15 @@
+const myFunction = () => {
+
+    const x = document.getElementById("header__links");
+
+    if (x.style.display === "block") {
+
+        x.style.display = "none";
+
+    } else {
+
+        x.style.display = "block";
+
+    }
+
+};
